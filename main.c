0d510fbe8f3492e1cd41cb6ee4b9bdb1b65627ee
adiy55 <adiyafe55@gmail.com>
2021-12-19 15:39:39 +0200

    Minor bug fix

--- main.c
+++ main.c
@@ -217,15 +217,13 @@ int textAnagram() {
 }
 
 void sort(char *arr) {
-    int i, j, flag = 1, temp;
-    for (i = 0; i < strlen(arr) - 1 && flag; i++) {
-        flag = 0;
+    int i, j, temp;
+    for (i = 0; i < strlen(arr) - 1; i++) {
         for (j = i + 1; j < strlen(arr); j++) {
             if (arr[i] > arr[j]) {
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-                flag = 1;
             }
         }
     }
